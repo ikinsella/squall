7601ffb3e913e39c5601827b5f40dc9e3d5f7017
Ian Kinsella <ikinsella@wisc.edu>
2015-10-12 01:46:20 -0500

    Added emacs configuration to ec2 setup.sh script

--- ec2/templates/setup.sh
+++ ec2/templates/setup.sh
@@ -25,3 +25,84 @@ curl -sSL https://get.docker.com/ubuntu/ | sh
 
 # orchestrates docker containers
 pip install docker-compose
+
+# Build Emacs 24.4 From Source
+apt-get -y install build-essential
+sudo apt-get build-dep emacs24
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
+tar -xzvf emacs-24.4.tar.gz
+rm emacs-24.4.tar.gz
+cd emacs-24.4
+./configure --prefix=/opt/emacs
+make
+make install
+cp /src/emacs /usr/bin/emacs
+
+# Create Directory For Emacs Config
+cd ~
+mkdir .emacs.d
+
+# Python Packages Required For Elpy
+pip install \
+    jedi \
+    flake8 \
+    importmagic \
+    autopep8
+
+# Create init.el
+echo ";; Tell Init To Install Required Packages If Not Found
+(load \"~/.emacs.d/init-packages\")
+" >> .emacs.d/init.el
+
+echo "
+;; Auto Initialize Elpy
+(package-initialize)
+(elpy-enable)
+" >> .emacs.d/init.el
+
+echo "
+;; Fixing a key binding bug in elpy
+(define-key yas-minor-mode-map (kbd \"C-c k\") 'yas-expand)
+;; Fixing another key binding bug in iedit mode
+(define-key global-map (kbd \"C-c o\") 'iedit-mode)
+(elpy-use-ipython)
+" >> .emacs.d/init.el
+
+# Create init-packages.el
+echo "
+(require 'package)
+
+(add-to-list 'package-archives
+             '(\"elpy\" . \"http://jorgenschaefer.github.io/packages/\"))
+
+(add-to-list 'package-archives
+             '(\"marmalade\" . \"http://marmalade-repo.org/packages/\"))
+
+(add-to-list 'package-archives
+             '(\"melpa-stable\" . \"http://melpa-stable.milkbox.net/packages/\") t)
+
+(add-to-list 'load-path \"~/.emacs.d/site-lisp/\")
+
+
+; list the packages you want
+(setq package-list
+    '(python-environment deferred epc 
+        flycheck ctable jedi concurrent company cyberpunk-theme elpy 
+        yasnippet pyvenv highlight-indentation find-file-in-project 
+        sql-indent sql exec-path-from-shell iedit
+        auto-complete popup let-alist magit
+        minimap popup))
+
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+" >> .emacs.d/init-packages.el
