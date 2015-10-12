ccbfc6700eba37524406b6bf6d3f572f4d754331
Ian Kinsella <ikinsella@wisc.edu>
2015-10-11 20:59:43 -0500

    pushed app updates

--- docker/app.py
+++ docker/app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 import datetime
 import os
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Make connection to DB container
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
+db = client.post_database
+
+"""
 @app.route('/')
 def hello():
     client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
-    #  mongo = MongoClient('mongodb://192.168.99.100:27017/')
     db = client.post_database
     posts = db.posts
     post = {"Hello": "Hello World!",
@@ -21,6 +25,20 @@ def hello():
     #  newest_post = posts.find_one({"_id": str_id})
     #  return str(posts.find_one({"_id": newest_post_id}))
     return mystr
+"""
 
+@app.route('/')
+def todo():
+    _experiments = db.experiments.find()
+    experiments = [experiment for experiment in _experiments]
+    return render_template('squall.html', items=experiments)
+
+@app.route('/new', methods=['POST'])
+def new():
+    item_doc = {'name': request.form['name'],
+                'description': request.form['description']}
+    db.experiments.insert_one(item_doc)
+    return redirect(url_for('todo'))
+            
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
