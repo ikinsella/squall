a144374efff47fef91c46b262583bd1de0866871
Ian Kinsella <ikinsella@wisc.edu>
2016-02-27 22:04:47 -0600

    Beginning to add some documentation to the pipeline

--- compile_batch.sh
+++ compile_batch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Usage: 1st arg name of batch directory followin standard format
+# copy batch directory to submit node
 scp -r $1/ ikinsella@submit-5.chtc.wisc.edu:~/
+# On the Submit Node: Use make file to tarzip sourcecode and send it to a build node in an interactive job
+# On the Build Node: Use script and make file to automate compilation, cleanup. tarzipped binaries will be sent back to submit node
 ssh ikinsella@submit-5.chtc.wisc.edu "cd $1; make sdist; condor_submit -i interactive_build.sub < ./condor_compile.sh"
-scp ikinsella@submit-5.chtc.wisc.edu:~/$1/binaries.tar.gz $1/ 
+# Copy tarzipped binaries from submit node to working directory
+scp ikinsella@submit-5.chtc.wisc.edu:~/$1/binaries.tar.gz $1/
+# Remove everything which was added to the submit node
+ssh ikinsella@submit-5.chtc.wisc.edu "rm -rf $1"
