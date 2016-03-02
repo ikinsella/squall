80732387a94311a89f517187ba53bfc1b1ed99a2
Ian Kinsella <ikinsella@wisc.edu>
2016-03-02 01:03:06 -0600

    Automating python pipeline and improving matlab pipeline

---
+++ Examples/MATLAB/ikinsella@submit-5.chtc.wisc.edu/dependencies/setupMCR/setupR2011b.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Setup Matlab Runtime Environment on Condor
+#
+echo "------------------------------------------"
+echo Setting up environment variables
+MCRROOT="v716"
+echo ---
+LD_LIBRARY_PATH=.:${MCRROOT}/runtime/glnxa64 ;
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/bin/glnxa64 ;
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/os/glnxa64;
+XAPPLRESDIR=${MCRROOT}/X11/app-defaults ;
+export XAPPLRESDIR;
+export LD_LIBRARY_PATH;
+echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
+echo "------------------------------------------"
