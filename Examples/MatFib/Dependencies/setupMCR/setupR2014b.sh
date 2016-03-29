fd9178360daea6475bef0c42ed748943af860f01
Ian Kinsella <ikinsella@wisc.edu>
2016-03-29 17:53:50 -0500

    Updating downloads to verify integrity using .md5 hashes & patching missing libraries for MATLAB setup scripts

--- Examples/MatFib/Dependencies/setupMCR/setupR2014b.sh
+++ Examples/MatFib/Dependencies/setupMCR/setupR2014b.sh
@@ -9,6 +9,7 @@ echo ---
 LD_LIBRARY_PATH=.:${MCRROOT}/runtime/glnxa64 ;
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/bin/glnxa64 ;
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/os/glnxa64;
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:"./lib64"
 export LD_LIBRARY_PATH;
 echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
 echo "------------------------------------------"
